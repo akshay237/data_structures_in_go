8b26d1f52f60ed39d4d6b918851333a57b98b14e
aks <aks@victoria.com>
2023-12-30 21:33:16 +0530

    stacks done

--- stack/stack.go
+++ stack/stack.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"slices"
 )
 
@@ -156,6 +157,204 @@ func NSL(arr []int) []int {
 	return ans
 }
 
+// 5. Stock Span Problem where we have given price of a stock for each day we have to diff consecutive no of days the price of stock is less than today
+func stockSpan(arr []int) []int {
+	type tuple struct {
+		idx   int
+		value int
+	}
+	stack := make([]tuple, 0)
+	ans := make([]int, 0)
+	for i := 0; i < len(arr); i++ {
+		for len(stack) > 0 && stack[len(stack)-1].value < arr[i] {
+			index := len(stack) - 1
+			stack = slices.Delete(stack, index, index+1)
+		}
+		if len(stack) == 0 {
+			ans = append(ans, -1)
+		} else {
+			ans = append(ans, stack[len(stack)-1].idx)
+		}
+		t := tuple{
+			idx:   i,
+			value: arr[i],
+		}
+		stack = append(stack, t)
+	}
+	return ans
+}
+
+// 6. Max area of histogram
+/*
+We have given an array height where each index holds height of a building and if we form building with this height and width 1 we will get a histogram and we have to print the
+maximum area out of it.
+heights = [6, 2, 5, 4, 5, 1, 6]
+out = 12
+We have to find the width by finding the NSR and NSL of indexs
+*/
+
+func NearestSmallestRight(arr []int) []int {
+	type tuple struct {
+		idx   int
+		value int
+	}
+	stack := make([]tuple, 0)
+	ans := make([]int, len(arr))
+	for i := len(arr) - 1; i >= 0; i-- {
+		for len(stack) > 0 && stack[len(stack)-1].value >= arr[i] {
+			index := len(stack) - 1
+			stack = slices.Delete(stack, index, index+1)
+		}
+		if len(stack) == 0 {
+			ans[i] = len(arr)
+		} else {
+			ans[i] = stack[len(stack)-1].idx
+		}
+		t := tuple{
+			idx:   i,
+			value: arr[i],
+		}
+		stack = append(stack, t)
+	}
+	return ans
+}
+
+func NearestSmallestLeft(arr []int) []int {
+	type tuple struct {
+		idx   int
+		value int
+	}
+	stack := make([]tuple, 0)
+	ans := make([]int, 0)
+	for i := 0; i < len(arr); i++ {
+		for len(stack) > 0 && stack[len(stack)-1].value >= arr[i] {
+			index := len(stack) - 1
+			stack = slices.Delete(stack, index, index+1)
+		}
+		if len(stack) == 0 {
+			ans = append(ans, -1)
+		} else {
+			ans = append(ans, stack[len(stack)-1].idx)
+		}
+		t := tuple{
+			idx:   i,
+			value: arr[i],
+		}
+		stack = append(stack, t)
+	}
+	return ans
+}
+
+func MAH(height []int) int {
+	// 1. stores indexes of smallest element from left
+	left := NearestSmallestLeft(height)
+	//fmt.Println("Left is:", left)
+	// 2. stores indexes of smallest element from right
+	right := NearestSmallestRight(height)
+	// fmt.Println("Right is:", right)
+	// 3. find the width
+	width := make([]int, len(height))
+	for idx, _ := range height {
+		width[idx] = right[idx] - left[idx] - 1
+	}
+	// 4. find the area
+	var max_area int
+	for idx, _ := range height {
+		max_area = int(math.Max(float64(max_area), float64(height[idx]*width[idx])))
+	}
+	return max_area
+}
+
+// 7. Find max area in a binary matrix
+func maxAreaInMatrix(mat [][]int) int {
+	max_area := 0
+	height := make([]int, len(mat[0]))
+	for i := 0; i < len(mat); i++ {
+		for j := 0; j < len(mat[0]); j++ {
+			if mat[i][j] == 0 {
+				height[j] = 0
+			} else {
+				height[j] = height[j] + mat[i][j]
+			}
+		}
+		fmt.Println("Height is:", height)
+		area := MAH(height)
+		fmt.Println("Area of each histogram is:", area)
+		max_area = int(math.Max(float64(max_area), float64(area)))
+	}
+	return max_area
+}
+
+// 8. Rain Water Tapping Problem
+func rainTapWater(arr []int) int {
+	maxL := make([]int, len(arr))
+	maxR := make([]int, len(arr))
+
+	// assign first and last value from arr
+	maxL[0] = arr[0]
+	maxR[len(arr)-1] = arr[len(arr)-1]
+
+	for i := 1; i < len(arr); i++ {
+		maxL[i] = int(math.Max(float64(maxL[i-1]), float64(arr[i])))
+	}
+
+	for i := len(arr) - 2; i >= 0; i-- {
+		maxR[i] = int(math.Max(float64(maxR[i+1]), float64(arr[i])))
+	}
+
+	waterUnits := 0
+	for i := 0; i < len(arr); i++ {
+		unit := int(math.Min(float64(maxL[i]), float64(maxR[i]))) - arr[i]
+		waterUnits += unit
+	}
+	return waterUnits
+}
+
+// 9. Min Stack with O(1) space
+type MinStack struct {
+	st     []int
+	minEle int
+}
+
+func (m *MinStack) push(ele int) []int {
+	if len(m.st) == 0 {
+		m.minEle = ele
+		m.st = append(m.st, ele)
+	} else {
+		if ele > m.minEle {
+			m.st = append(m.st, ele)
+		} else {
+			m.st = append(m.st, 2*ele-m.minEle)
+			m.minEle = ele
+		}
+	}
+	return m.st
+}
+
+func (m *MinStack) pop() int {
+	if len(m.st) == 0 {
+		fmt.Println("min stack is empty")
+		return -1
+	} else {
+		idx := len(m.st) - 1
+		val := m.st[idx]
+		m.st = slices.Delete(m.st, idx, idx+1)
+		if val < m.minEle {
+			e := m.minEle
+			m.minEle = 2*m.minEle - val
+			return e
+		}
+		return val
+	}
+}
+
+func NewMinStack() *MinStack {
+	return &MinStack{
+		st:     make([]int, 0),
+		minEle: 0,
+	}
+}
+
 func main() {
 
 	// 1. find NGR of an array
@@ -174,4 +373,42 @@ func main() {
 	// 4. find the NSL of an array
 	nsl := NSL(arr)
 	fmt.Println("NSL of arr is:", nsl)
+
+	// 5. stock span problem
+	arr = []int{100, 80, 60, 70, 60, 75, 85}
+	span := stockSpan(arr)
+	for i := 0; i < len(span); i++ {
+		span[i] = i - span[i]
+	}
+	fmt.Println("Stock span days are:", span)
+
+	// 6. find maximum area of histogram
+	height := []int{6, 2, 5, 4, 5, 1, 6}
+	max_area := MAH(height)
+	fmt.Println("Maximum area of histogram is:", max_area)
+
+	// 7. max area in a binary matrix
+	matrix := [][]int{{0, 1, 1, 0}, {1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 0, 0}}
+	max_area = maxAreaInMatrix(matrix)
+	fmt.Println("Max area in a binary matrix is: ", max_area)
+
+	// 8. rain water tapping
+	height = []int{3, 0, 0, 2, 0, 4}
+	waterUnits := rainTapWater(height)
+	fmt.Println("Water unit tapped are:", waterUnits)
+
+	// 9. implement stack with o(1)
+	m := NewMinStack()
+	ms := m.push(9)
+	ms = m.push(7)
+	ms = m.push(10)
+	fmt.Println("Min Stack: ", ms)
+	fmt.Println("Min Ele: ", m.minEle)
+	p := m.pop()
+	fmt.Println("Popped Ele:", p)
+	fmt.Println("Min Stack after ele popped:", m.st)
+	fmt.Println("Min Ele is:", m.minEle)
+	ms = m.push(3)
+	fmt.Println("Min Stack: ", ms)
+	fmt.Println("Min Ele: ", m.minEle)
 }
